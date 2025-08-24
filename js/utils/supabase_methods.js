1b0836db4d012b1df5e2f5d93d463c427cef3930
Victor Schweitz <victorschweitz@hotmail.com>
2025-08-24 18:28:39 +0200

    Fetch calls collected in one file, from where they are being exported and imported into the needed files.

---
+++ js/utils/supabase_methods.js
@@ -0,0 +1,35 @@
+import { supabaseClient } from "../supabase/supabase_client.js"
+
+// Fetch posts
+export const fetchAllPosts = async () =>
+{
+    const { data: postsData, error: postsError } = await supabaseClient
+      .from("blog_posts")
+      .select("*")
+      .order('publish_date', {ascending: true})
+
+    if (postsError)
+    {
+        throw new Error(`Failed to fetch posts: ${postsError.message}`)
+    }
+    
+    return postsData || []
+}
+
+// Fetch single posts by slug
+export const fetchSinglePostBySlug = async (slug) =>
+{
+    const { data: post, error } = await supabaseClient
+        .from("blog_posts")
+        .select("*")
+        .eq("post_slug", slug)
+        .single()
+    
+
+    if (error)
+    {
+        console.log("Error: ", error.hint, error.message, slug)
+    }
+    
+    return post || []
+}
