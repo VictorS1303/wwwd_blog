fec8549de27fc86d2c69813f893bd69873f3fdc3
Victor Schweitz <victorschweitz@hotmail.com>
2025-09-17 18:27:48 +0200

    Sign up done.

--- js/supabase/supabase_methods.js
+++ js/supabase/supabase_methods.js
@@ -1,24 +1,49 @@
 import {supabaseClient} from './supabase_client.js'
 
 // Sign up user
-export const signUpUser = async (email, password) =>
-{
-    const {data: signUpData, error: signUpError} = await supabaseClient.auth.signUp({
-        email, password
+export const signUpUser = async (name, email, password, avatarUrl = null) => {
+  try {
+    const { data, error } = await supabaseClient.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          name,
+          ...(avatarUrl && { avatarUrl }), // only include if provided
+        },
+      },
     })
 
-    if(signUpError)
-    {
-        console.log('Error signing up: ', signUpError.hint, signUpError.message)
-        
-        return {success: false, signUpError: signUpError.message }
+    console.log("Sign up data:", data)
+    console.log("Sign up error:", error)
+
+    if (error) {
+      return { success: false, signUpError: error.message }
     }
 
-    const session = supabaseClient.auth.getSession()
-
-    return {
-        success: true,
-        user: signUpData.user,
-        session,
-    }
+    return { success: true, user: data.user, session: data.session }
+  } catch (err) {
+    return { success: false, signUpError: err.message }
+  }
 }
+
+// const email = `${email.replace(/\+s/g, '').toLowerCase()}`
+
+// const {data: signUpData, error: signUpError} = await supabaseClient.auth.signUp({
+//     email, password
+// })
+
+// if(signUpError)
+// {
+//     console.log('Error signing up: ', signUpError.hint, signUpError.message)
+    
+//     return {success: false, signUpError: signUpError.message }
+// }
+
+// const session = supabaseClient.auth.getSession()
+
+// return {
+//     success: true,
+//     user: signUpData.user,
+//     session,
+// }
